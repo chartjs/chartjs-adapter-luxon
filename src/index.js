a19e5b54029aada4db0ad2e8ab818807afaba6bf
Jon Rimmer <jon.rimmer@gmail.com>
2019-03-17 15:05:14 +0000

    Pass through Luxon options

--- src/index.js
+++ src/index.js
@@ -14,62 +14,71 @@ var FORMATS = {
 	year: 'yyyy'
 };
 
-function create(time) {
-	return DateTime.fromMillis(time);
-}
-
 _adapters._date.override({
 	_id: 'luxon', // DEBUG
+
+	/**
+	 * @private
+	 */
+	_create(time) {
+		return DateTime.fromMillis(time, this.options);
+	},
 
 	formats: function() {
 		return FORMATS;
 	},
 
 	parse: function(value, format) {
+		var me = this;
+
 		if (helpers.isNullOrUndef(value)) {
 			return null;
 		}
 
 		var type = typeof value;
 		if (type === 'number') {
-			value = create(value);
+			value = me._create(value);
 		} else if (type === 'string') {
 			if (typeof format === 'string') {
-				value = DateTime.fromFormat(value, format);
+				value = DateTime.fromFormat(value, format, me.options);
 			} else {
-				value = DateTime.fromISO(value);
+				value = DateTime.fromISO(value, me.options);
 			}
 		} else if (type === 'object') {
 			value = DateTime.fromObject(value);
 		} else if (value instanceof Date) {
-			value = DateTime.fromJSDate(value);
+			value = DateTime.fromJSDate(value, me.options);
 		}
 
 		return value.isValid ? value.valueOf() : null;
 	},
 
 	format: function(time, format) {
-		return create(time).toFormat(format);
+		var me = this;
+		return me._create(time).toFormat(format, me.options);
 	},
 
 	add: function(time, amount, unit) {
 		var args = {};
 		args[unit] = amount;
-		return create(time).plus(args).valueOf();
+		return this._create(time).plus(args).valueOf();
 	},
 
 	diff: function(max, min, unit) {
-		return create(max).diff(create(min)).as(unit).valueOf();
+		var me = this;
+		return me._create(max).diff(me._create(min)).as(unit).valueOf();
 	},
 
 	startOf: function(time, unit, weekday) {
+		var me = this;
+
 		if (unit === 'isoWeek') {
-			return create(time).isoWeekday(weekday).valueOf();
+			return me._create(time).isoWeekday(weekday).valueOf();
 		}
-		return unit ? create(time).startOf(unit).valueOf() : time;
+		return unit ? me._create(time).startOf(unit).valueOf() : time;
 	},
 
 	endOf: function(time, unit) {
-		return create(time).endOf(unit).valueOf();
+		return this._create(time).endOf(unit).valueOf();
 	}
 });
