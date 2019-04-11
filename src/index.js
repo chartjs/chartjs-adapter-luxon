182f90aeb7dc879a011685d721f4ae1975ba121a
Jon Rimmer <jon.rimmer@gmail.com>
2019-04-11 07:17:04 +0100

    Add support for adapter options (#5)

--- src/index.js
+++ src/index.js
@@ -14,62 +14,67 @@ var FORMATS = {
 	year: 'yyyy'
 };
 
-function create(time) {
-	return DateTime.fromMillis(time);
-}
-
 _adapters._date.override({
 	_id: 'luxon', // DEBUG
+
+	/**
+	 * @private
+	 */
+	_create: function(time) {
+		return DateTime.fromMillis(time, this.options);
+	},
 
 	formats: function() {
 		return FORMATS;
 	},
 
 	parse: function(value, format) {
+		var options = this.options;
+
 		if (helpers.isNullOrUndef(value)) {
 			return null;
 		}
 
 		var type = typeof value;
 		if (type === 'number') {
-			value = create(value);
+			value = this._create(value);
 		} else if (type === 'string') {
 			if (typeof format === 'string') {
-				value = DateTime.fromFormat(value, format);
+				value = DateTime.fromFormat(value, format, options);
 			} else {
-				value = DateTime.fromISO(value);
+				value = DateTime.fromISO(value, options);
 			}
 		} else if (type === 'object') {
 			value = DateTime.fromObject(value);
 		} else if (value instanceof Date) {
-			value = DateTime.fromJSDate(value);
+			value = DateTime.fromJSDate(value, options);
 		}
 
 		return value.isValid ? value.valueOf() : null;
 	},
 
 	format: function(time, format) {
-		return create(time).toFormat(format);
+		return this._create(time).toFormat(format, this.options);
 	},
 
 	add: function(time, amount, unit) {
 		var args = {};
 		args[unit] = amount;
-		return create(time).plus(args).valueOf();
+		return this._create(time).plus(args).valueOf();
 	},
 
 	diff: function(max, min, unit) {
-		return create(max).diff(create(min)).as(unit).valueOf();
+		return this._create(max).diff(this._create(min)).as(unit).valueOf();
 	},
 
 	startOf: function(time, unit, weekday) {
 		if (unit === 'isoWeek') {
-			return create(time).isoWeekday(weekday).valueOf();
+			return this._create(time).isoWeekday(weekday).valueOf();
 		}
-		return unit ? create(time).startOf(unit).valueOf() : time;
+		return unit ? this._create(time).startOf(unit).valueOf() : time;
 	},
 
 	endOf: function(time, unit) {
-		return create(time).endOf(unit).valueOf();
+		return this._create(time).endOf(unit).valueOf();
 	}
 });
---
+++ test/specs/luxon-adapter.spec.js
@@ -0,0 +1,39 @@
+describe('Luxon Adapter', function() {
+	it('should accept timezone configuration', function() {
+		var chart = jasmine.chart.acquire({
+			type: 'line',
+			data: {
+				datasets: [{
+					data: [{
+						t: 0,
+						y: 100
+					}]
+				}]
+			},
+			options: {
+				scales: {
+					xAxes: [{
+						type: 'time',
+						id: 'xAxis0',
+						time: {
+							unit: 'second',
+							displayFormats: {
+								second: 'MMM d, yyyy, h:mm:ss a'
+							}
+						},
+						adapters: {
+							date: {
+								zone: 'UTC+6'
+							}
+						},
+						ticks: {
+							source: 'data'
+						}
+					}]
+				}
+			}
+		});
+
+		expect(chart.scales.xAxis0.ticks[0]).toEqual('Jan 1, 1970, 6:00:00 AM');
+	});
+});
