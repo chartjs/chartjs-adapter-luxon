7e7fe564e9ea6183c9321f146b36a59002e8775a
Colin Griffin <colin@krum.io>
2020-04-10 15:30:01 -0400

    update rollup to output esm and have feature parity with charjs rollup (#16)

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,11 @@
+/* eslint-disable import/no-commonjs */
+/* eslint-env es6 */
+
+
+const babel = require('rollup-plugin-babel');
+const cleanup = require('rollup-plugin-cleanup');
+const json = require('@rollup/plugin-json');
+const resolve = require('@rollup/plugin-node-resolve');
 const terser = require('rollup-plugin-terser').terser;
 const pkg = require('./package.json');
 
@@ -8,9 +16,19 @@ const banner = `/*!
  * Released under the ${pkg.license} license
  */`;
 
+const input = 'src/index.js';
+
 module.exports = [
 	{
-		input: 'src/index.js',
+		input,
+		plugins: [
+			json(),
+			resolve(),
+			babel(),
+			cleanup({
+				sourcemap: true
+			})
+		],
 		output: {
 			file: `dist/${pkg.name}.js`,
 			banner: banner,
@@ -27,7 +45,17 @@ module.exports = [
 		]
 	},
 	{
-		input: 'src/index.js',
+		input,
+		plugins: [
+			json(),
+			resolve(),
+			babel(),
+			terser({
+				output: {
+					preamble: banner
+				}
+			})
+		],
 		output: {
 			file: `dist/${pkg.name}.min.js`,
 			format: 'umd',
@@ -37,16 +65,56 @@ module.exports = [
 				luxon: 'luxon'
 			}
 		},
+		external: [
+			'chart.js',
+			'luxon'
+		]
+	},
+
+	// ES6 builds
+	// dist/Chart.esm.min.js
+	// dist/Chart.esm.js
+	{
+		input,
 		plugins: [
+			json(),
+			resolve(),
+			babel({envName: 'es6'}),
+			cleanup({
+				sourcemap: true
+			})
+		],
+		output: {
+			file: `dist/${pkg.name}.esm.js`,
+			banner,
+			format: 'esm',
+			indent: false,
+		},
+		external: [
+			'chart.js',
+			'luxon'
+		]
+	},
+	{
+		input,
+		plugins: [
+			json(),
+			resolve(),
+			babel({envName: 'es6'}),
 			terser({
 				output: {
 					preamble: banner
 				}
 			})
 		],
+		output: {
+			file: 'dist/${pkg.name}.esm.min.js',
+			format: 'esm',
+			indent: false,
+		},
 		external: [
 			'chart.js',
 			'luxon'
 		]
-	}
+	},
 ];
