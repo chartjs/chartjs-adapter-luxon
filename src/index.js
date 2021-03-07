a23798ead848c904a06f3733390ea9b99bbff304
Evert Timberg <evert.timberg+github@gmail.com>
2021-03-07 14:29:35 -0500

    Move CI to GitHub Actions + Update for Chart.js v3 (#29)
    * Replace gulp as build system
    
    * Move from Travis to GitHub Actions
    
    * Code linting and testing
    
    * Update release drafter action
    
    * Remove debug log statement
    
    * Remove dry-run

--- src/index.js
+++ src/index.js
@@ -1,85 +1,85 @@
 import Chart from 'chart.js';
-import { DateTime } from 'luxon';
+import {DateTime} from 'luxon';
 
 const FORMATS = {
-	datetime: DateTime.DATETIME_MED_WITH_SECONDS,
-	millisecond: 'h:mm:ss.SSS a',
-	second: DateTime.TIME_WITH_SECONDS,
-	minute: DateTime.TIME_SIMPLE,
-	hour: { hour: 'numeric' },
-	day: { day: 'numeric', month: 'short' },
-	week: 'DD',
-	month: { month: 'short', year: 'numeric' },
-	quarter: "'Q'q - yyyy",
-	year: { year: 'numeric' }
+  datetime: DateTime.DATETIME_MED_WITH_SECONDS,
+  millisecond: 'h:mm:ss.SSS a',
+  second: DateTime.TIME_WITH_SECONDS,
+  minute: DateTime.TIME_SIMPLE,
+  hour: {hour: 'numeric'},
+  day: {day: 'numeric', month: 'short'},
+  week: 'DD',
+  month: {month: 'short', year: 'numeric'},
+  quarter: "'Q'q - yyyy",
+  year: {year: 'numeric'}
 };
 
 Chart._adapters._date.override({
-	_id: 'luxon', // DEBUG
+  _id: 'luxon', // DEBUG
 
-	/**
-	 * @private
-	 */
-	_create: function(time) {
-		return DateTime.fromMillis(time, this.options);
-	},
+  /**
+   * @private
+   */
+  _create: function(time) {
+    return DateTime.fromMillis(time, this.options);
+  },
 
-	formats: function() {
-		return FORMATS;
-	},
+  formats: function() {
+    return FORMATS;
+  },
 
-	parse: function(value, format) {
-		const options = this.options;
+  parse: function(value, format) {
+    const options = this.options;
 
-		if (value === null || typeof value === 'undefined') {
-			return null;
-		}
+    if (value === null || typeof value === 'undefined') {
+      return null;
+    }
 
-		const type = typeof value;
-		if (type === 'number') {
-			value = this._create(value);
-		} else if (type === 'string') {
-			if (typeof format === 'string') {
-				value = DateTime.fromFormat(value, format, options);
-			} else {
-				value = DateTime.fromISO(value, options);
-			}
-		} else if (type === 'object' && !(value instanceof DateTime)) {
-			value = DateTime.fromObject(value);
-		} else if (value instanceof Date) {
-			value = DateTime.fromJSDate(value, options);
-		}
+    const type = typeof value;
+    if (type === 'number') {
+      value = this._create(value);
+    } else if (type === 'string') {
+      if (typeof format === 'string') {
+        value = DateTime.fromFormat(value, format, options);
+      } else {
+        value = DateTime.fromISO(value, options);
+      }
+    } else if (type === 'object' && !(value instanceof DateTime)) {
+      value = DateTime.fromObject(value);
+    } else if (value instanceof Date) {
+      value = DateTime.fromJSDate(value, options);
+    }
 
-		return value.isValid ? value.valueOf() : null;
-	},
+    return value.isValid ? value.valueOf() : null;
+  },
 
-	format: function(time, format) {
-		const datetime = this._create(time);
-		return typeof format === 'string'
-			? datetime.toFormat(format, this.options)
-			: datetime.toLocaleString(format);
-	},
+  format: function(time, format) {
+    const datetime = this._create(time);
+    return typeof format === 'string'
+      ? datetime.toFormat(format, this.options)
+      : datetime.toLocaleString(format);
+  },
 
-	add: function(time, amount, unit) {
-		const args = {};
-		args[unit] = amount;
-		return this._create(time).plus(args).valueOf();
-	},
+  add: function(time, amount, unit) {
+    const args = {};
+    args[unit] = amount;
+    return this._create(time).plus(args).valueOf();
+  },
 
-	diff: function(max, min, unit) {
-		return this._create(max).diff(this._create(min)).as(unit).valueOf();
-	},
+  diff: function(max, min, unit) {
+    return this._create(max).diff(this._create(min)).as(unit).valueOf();
+  },
 
-	startOf: function(time, unit, weekday) {
-		if (unit === 'isoWeek') {
-			weekday = Math.trunc(Math.min(Math.max(0, weekday), 6));
-			const dateTime = this._create(time);
-			return dateTime.minus({ days: (dateTime.weekday < weekday ? 7 : 0) + dateTime.weekday - weekday }).startOf('day').valueOf();
-		}
-		return unit ? this._create(time).startOf(unit).valueOf() : time;
-	},
+  startOf: function(time, unit, weekday) {
+    if (unit === 'isoWeek') {
+      weekday = Math.trunc(Math.min(Math.max(0, weekday), 6));
+      const dateTime = this._create(time);
+      return dateTime.minus({days: (dateTime.weekday < weekday ? 7 : 0) + dateTime.weekday - weekday}).startOf('day').valueOf();
+    }
+    return unit ? this._create(time).startOf(unit).valueOf() : time;
+  },
 
-	endOf: function(time, unit) {
-		return this._create(time).endOf(unit).valueOf();
-	}
+  endOf: function(time, unit) {
+    return this._create(time).endOf(unit).valueOf();
+  }
 });
--- test/specs/luxon.spec.js
+++ test/specs/luxon.spec.js
@@ -1,28 +1,28 @@
-import { DateTime } from 'luxon';
+const {DateTime} = luxon;
 
 var options = {
-	zone: 'UTC+1',
-	locale: 'en-GB',
-	numberingSystem: 'latn',
-	outputCalendar: 'gregory'
+  zone: 'UTC+1',
+  locale: 'en-GB',
+  numberingSystem: 'latn',
+  outputCalendar: 'gregory'
 };
 
 describe('Luxon', function() {
-	it('should accept complete options for each options accepting method used', function() {
-		expect(function() {
-			DateTime.fromMillis(0, options).toISO();
-		}).not.toThrow();
-		expect(function() {
-			DateTime.fromFormat('01', 'DD', options);
-		}).not.toThrow();
-		expect(function() {
-			DateTime.fromISO('1970-01-01T00:00:00.000Z', options);
-		}).not.toThrow();
-		expect(function() {
-			DateTime.fromJSDate(new Date(0), options);
-		}).not.toThrow();
-		expect(function() {
-			DateTime.fromMillis(0, options).toFormat('DD', options);
-		}).not.toThrow();
-	});
+  it('should accept complete options for each options accepting method used', function() {
+    expect(function() {
+      DateTime.fromMillis(0, options).toISO();
+    }).not.toThrow();
+    expect(function() {
+      DateTime.fromFormat('01', 'DD', options);
+    }).not.toThrow();
+    expect(function() {
+      DateTime.fromISO('1970-01-01T00:00:00.000Z', options);
+    }).not.toThrow();
+    expect(function() {
+      DateTime.fromJSDate(new Date(0), options);
+    }).not.toThrow();
+    expect(function() {
+      DateTime.fromMillis(0, options).toFormat('DD', options);
+    }).not.toThrow();
+  });
 });
