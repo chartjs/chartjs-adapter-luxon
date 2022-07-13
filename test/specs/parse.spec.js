9e20dafa7a7e66da32b2ef1a11955c3ca641cb33
stockiNail <stocki.nail@gmail.com>
2022-07-13 19:22:56 +0200

    apply options to DateTime for parsing

--- test/specs/parse.spec.js
+++ test/specs/parse.spec.js
@@ -16,15 +16,16 @@ describe('Luxon Adapter', function() {
   }
 
   it('should parse correctly using different types of value', function() {
+    const options = {zone: 'UTC', locale: 'en-US'};
     const inputValues = [
       1559056227321, // UTC value
       new Date(1559056227321), // JS date with UTC
       new Date('2019-05-28T15:10:27.321Z'), // JS date with ISO
-      DateTime.fromMillis(1559056227321), // Luxon UTC
-      DateTime.fromISO('2019-05-28T15:10:27.321Z'), // Luxon ISO
+      DateTime.fromMillis(1559056227321, options), // Luxon UTC
+      DateTime.fromISO('2019-05-28T15:10:27.321Z', options), // Luxon ISO
       {year: 2019, month: 5, day: 28, hour: 17, minute: 10, second: 27, millisecond: 321} // INTL object
     ];
-    const adapter = new Chart._adapters._date({zone: 'UTC'});
+    const adapter = new Chart._adapters._date(options);
     for (const item of inputValues) {
       expect(adapter.parse(item)).withContext(`item: ${item}`).toEqual(1559056227321);
     }
