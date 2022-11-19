bdc72c1c5495f4e9153ab2ffb6ee8686dd52f26d
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-11-19 12:27:22 -0800

    chore: update packaging to match main chart.js library (#79)

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,10 +1,11 @@
 const istanbul = require('rollup-plugin-istanbul');
 const resolve = require('@rollup/plugin-node-resolve').default;
-const builds = require('./rollup.config');
 const yargs = require('yargs');
 const env = process.env.NODE_ENV;
 
-module.exports = function(karma) {
+module.exports = async function(karma) {
+  const builds = (await import('./rollup.config.js')).default;
+
   const args = yargs
     .option('verbose', {default: false})
     .argv;
@@ -13,7 +14,7 @@ module.exports = function(karma) {
   // we will prefer the unminified build which is easier to browse and works
   // better with source mapping. In other cases, pick the minified build to
   // make sure that the minification process (terser) doesn't break anything.
-  const regex = karma.autoWatch ? /chartjs-adapter-luxon\.js$/ : /chartjs-adapter-luxon\.min\.js$/;
+  const regex = karma.autoWatch ? /chartjs-adapter-luxon\.umd\.js$/ : /chartjs-adapter-luxon\.umd\.min\.js$/;
   const build = builds.filter(v => v.output.file && v.output.file.match(regex))[0];
 
   if (env === 'test') {
